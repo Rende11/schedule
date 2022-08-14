64d35a0b6071e0860ffe7af943d3b51d4d895de8
Rende11 <artamonovn@gmail.com>
2022-08-14 11:21:39 +0300

    Add base logic.

--- src/schedule/core.cljs
+++ src/schedule/core.cljs
@@ -1,40 +1,190 @@
 (ns schedule.core
-  (:require [reagent.dom :as rdom]
+  (:require [cljs.reader :as reader]
+            [cljs.tools.reader.edn :as edn]
+            [oz.core :as oz]
             [re-frame.core :as rf]
-            [oz.core :as oz]))
+            [reagent.core :as r]
+            [reagent.dom :as rdom]
+            [schedule.model :as sm]
+            [tick.core :as t]))
 
-(def sample-data
-  [{:power 1 :speed 2 :engine :v8}
-   {:power 3 :speed 4 :engine :v6}
-   {:power 8 :speed 7 :engine :v8}
-   {:power 4 :speed 3.5 :engine :v8}
-   {:power 5 :speed 4 :engine :v8}
-   {:power 1.2 :speed 3 :engine :v6}
-   {:power 3.2 :speed 3.8 :engine :v6}
-   {:power 7.5 :speed 7.2 :engine :v6}])
 
-(defn simple-vega-lite-example []
-  [oz/vega-lite
-   {:data {:values sample-data}
-    :mark {:type :point
-           :tooltip true}
-    :width 500
-    :height 400
-    :encoding {:x {:field :power}
-               :y {:field :speed}
-               :color {:field :engine}
-               :size {:value 80}}}])
+(defn chart []
+  (let [[sample-data _] @(rf/subscribe [::sm/text-area-filtered-edn-value])
+        selected-period @(rf/subscribe [::sm/selected-period])
+        timeunit (if (#{:next-week} selected-period)
+                    "utc dayhoursminutes"
+                    "utc hoursminutes")]
+    [:div
+     [oz/vega-lite
+      {:data {:values (or sample-data [])}
+       :width 600
+       :height 500
+       :mark {:type "bar"
+              :color "#5046e4"
+              ;; Tooltip works badly
+              :tooltip true}
+       :encoding {:x {:field :start
+                      :timeUnit timeunit
+                      :type :temporal
+                      :title "From"}
+                  :x2 {:field :end
+                       :timeUnit timeunit 
+                       :type :temporal
+                       :title "To"}
+                  :y {:field :user-id
+                      :title "User"}}}]]))
 
+(defn input-block
+  []
+  (let [[_ err] @(rf/subscribe [::sm/text-area-edn-value])]
+    [:div.m-5 {:class "min-w-[30%]"}
+     [:textarea.border-solid.border-2.border-indigo-600.rounded.p-2.w-full
+      {:style {"height" "90vh"}
+       :on-change (fn [e]
+                    (let [value (-> e .-target .-value )]
+                      (rf/dispatch [::sm/set-text-area-value value])))
+       :value @(rf/subscribe [::sm/text-area-value])}]
+     (when err
+       [:div err])]))
+
+(defn user-label-all []
+  (let [all-users-selected? @(rf/subscribe [::sm/all-users-selected?])]
+    [:button.p-1.my-1.border-solid.border-2.border-indigo-600.rounded
+     {:class (when all-users-selected?
+               "bg-indigo-600 text-white")
+      :on-click #(rf/dispatch [::sm/reset-user-filter])}
+     "All"]))
+
+(defn user-label [uid]
+  (let [selected-user? @(rf/subscribe [::sm/selected-user? uid])]
+    [:button.p-1.my-1.border-solid.border-2.border-indigo-600.rounded
+     {:class (when selected-user?
+               "bg-indigo-600 text-white")
+      :on-click #(rf/dispatch [::sm/set-user-filter uid])}
+     uid]))
+
+(defn select-user []
+  (let [user-ids @(rf/subscribe [::sm/user-ids])]
+    [:div.flex.flex-col
+     [user-label-all]
+     (for [uid user-ids]
+       ^{:key uid}
+       [user-label uid])]))
+
+(defn date-label [{:keys [label value]}]
+  (let [selected-period? @(rf/subscribe [::sm/selected-period? value])]
+    [:button.px-3.py-1.my-1.mx-3.border-solid.border-2.border-indigo-600.rounded
+     {:class (when selected-period?
+               "bg-indigo-600 text-white")
+      :on-click #(rf/dispatch [::sm/set-date-filter value])}
+     label]))
+
+(defn select-date []
+  [:div.ml-8.flex
+   [date-label {:label "Today"
+                :value :today}]
+   [date-label {:label "Tomorrow"
+                :value :tomorrow}]
+   [date-label {:label "Next week"
+                :value :next-week}]])
+
+(defn chart-bar []
+  [:div.m-5.flex
+   [select-user]
+   [:div
+    [select-date]
+    [chart]]])
 
 (defn app []
+  []
   [:div
-   [simple-vega-lite-example]])
-
+   [:div.flex
+    [input-block]
+    [chart-bar]]])
 
 (defn ^:dev/after-load render []
   (rdom/render [app] (.getElementById js/document "root")))
 
+(rf/reg-event-fx
+ ::initialise
+ (fn [_ _]
+   {:db {:filter {:user-id #{}
+                   :period :today}
+         :text-area
+         {:value
+          (str [{:start #inst "2022-08-14T10:00:00.000Z" :end #inst "2022-08-14T14:00:00.000Z" :user-id "Max"}
+                {:start #inst "2022-08-14T16:00:00.000Z" :end #inst "2022-08-14T18:00:00.000Z" :user-id "Max"}
+                {:start #inst "2022-08-15T12:00:00.000Z" :end #inst "2022-08-15T18:00:00.000Z" :user-id "Max"}
+                {:start #inst "2022-08-17T10:00:00.000Z" :end #inst "2022-08-17T18:00:00.000Z" :user-id "Max"}
+                {:start #inst "2022-08-18T10:00:00.000Z" :end #inst "2022-08-18T18:00:00.000Z" :user-id "Max"}
+
+                {:start #inst "2022-08-14T16:30:00.000Z" :end #inst "2022-08-14T17:30:00.000Z" :user-id "Oleg"}
+                {:start #inst "2022-08-15T16:30:00.000Z" :end #inst "2022-08-15T17:30:00.000Z" :user-id "Oleg"}
+                {:start #inst "2022-08-16T16:30:00.000Z" :end #inst "2022-08-16T17:30:00.000Z" :user-id "Oleg"}
+                
+                {:start #inst "2022-08-14T08:00:00.000Z" :end #inst "2022-08-14T10:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-08-14T11:00:00.000Z" :end #inst "2022-08-14T13:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-08-15T14:00:00.000Z" :end #inst "2022-08-15T15:45:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-08-15T16:00:00.000Z" :end #inst "2022-08-15T18:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-08-15T19:00:00.000Z" :end #inst "2022-08-15T20:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-09-16T10:30:00.000Z" :end #inst "2022-09-16T20:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-09-17T10:30:00.000Z" :end #inst "2022-09-17T20:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-09-18T10:30:00.000Z" :end #inst "2022-09-18T20:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-09-19T10:30:00.000Z" :end #inst "2022-09-19T20:00:00.000Z" :user-id "Ann"}
+                {:start #inst "2022-09-20T10:30:00.000Z" :end #inst "2022-09-20T20:00:00.000Z" :user-id "Ann"}
+
+                {:start #inst "2022-08-14T10:00:00.000Z" :end #inst "2022-08-14T14:00:00.000Z" :user-id "MadMax"}
+                {:start #inst "2022-08-14T16:00:00.000Z" :end #inst "2022-08-14T18:00:00.000Z" :user-id "MadMax"}
+                {:start #inst "2022-08-15T12:00:00.000Z" :end #inst "2022-08-15T18:00:00.000Z" :user-id "MadMax"}
+                {:start #inst "2022-08-17T10:00:00.000Z" :end #inst "2022-08-17T18:00:00.000Z" :user-id "MadMax"}
+                {:start #inst "2022-08-18T10:00:00.000Z" :end #inst "2022-08-18T18:00:00.000Z" :user-id "MadMax"}
+
+                {:start #inst "2022-08-14T16:30:00.000Z" :end #inst "2022-08-14T17:30:00.000Z" :user-id "Fernando"}
+                {:start #inst "2022-08-15T16:30:00.000Z" :end #inst "2022-08-15T17:30:00.000Z" :user-id "Fernando"}
+                {:start #inst "2022-08-16T16:30:00.000Z" :end #inst "2022-08-16T17:30:00.000Z" :user-id "Fernando"}
+                
+                {:start #inst "2022-08-14T08:00:00.000Z" :end #inst "2022-08-14T10:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-08-14T11:00:00.000Z" :end #inst "2022-08-14T13:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-08-15T14:00:00.000Z" :end #inst "2022-08-15T15:45:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-08-15T16:00:00.000Z" :end #inst "2022-08-15T18:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-08-15T19:00:00.000Z" :end #inst "2022-08-15T20:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-09-16T10:30:00.000Z" :end #inst "2022-09-16T20:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-09-17T10:30:00.000Z" :end #inst "2022-09-17T20:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-09-18T10:30:00.000Z" :end #inst "2022-09-18T20:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-09-19T10:30:00.000Z" :end #inst "2022-09-19T20:00:00.000Z" :user-id "Nataly"}
+                {:start #inst "2022-09-20T10:30:00.000Z" :end #inst "2022-09-20T20:00:00.000Z" :user-id "Nataly"}
+
+                {:start #inst "2022-08-14T10:00:00.000Z" :end #inst "2022-08-14T14:00:00.000Z" :user-id "Bruce"}
+                {:start #inst "2022-08-14T16:00:00.000Z" :end #inst "2022-08-14T18:00:00.000Z" :user-id "Bruce"}
+                {:start #inst "2022-08-15T12:00:00.000Z" :end #inst "2022-08-15T18:00:00.000Z" :user-id "Bruce"}
+                {:start #inst "2022-08-17T10:00:00.000Z" :end #inst "2022-08-17T18:00:00.000Z" :user-id "Bruce"}
+                {:start #inst "2022-08-18T10:00:00.000Z" :end #inst "2022-08-18T18:00:00.000Z" :user-id "Bruce"}
+
+                {:start #inst "2022-08-14T16:30:00.000Z" :end #inst "2022-08-14T17:30:00.000Z" :user-id "Luke"}
+                {:start #inst "2022-08-15T16:30:00.000Z" :end #inst "2022-08-15T17:30:00.000Z" :user-id "Luke"}
+                {:start #inst "2022-08-16T16:30:00.000Z" :end #inst "2022-08-16T17:30:00.000Z" :user-id "Luke"}
+                
+                {:start #inst "2022-08-14T08:00:00.000Z" :end #inst "2022-08-14T10:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-08-14T11:00:00.000Z" :end #inst "2022-08-14T13:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-08-15T14:00:00.000Z" :end #inst "2022-08-15T15:45:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-08-15T16:00:00.000Z" :end #inst "2022-08-15T18:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-08-15T19:00:00.000Z" :end #inst "2022-08-15T20:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-09-16T10:30:00.000Z" :end #inst "2022-09-16T20:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-09-17T10:30:00.000Z" :end #inst "2022-09-17T20:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-09-18T10:30:00.000Z" :end #inst "2022-09-18T20:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-09-19T10:30:00.000Z" :end #inst "2022-09-19T20:00:00.000Z" :user-id "Zena"}
+                {:start #inst "2022-09-20T10:30:00.000Z" :end #inst "2022-09-20T20:00:00.000Z" :user-id "Zena"}])}}}))
+
 
 (defn ^:export init
   []
+  (rf/dispatch-sync [::initialise])
   (render))
+
+(comment
+  (t/now)
+  [{:start "2022-08-12T10:00:00.000Z" :end "2022-08-12T14:00:00.000Z" :user-id "Max"}]
+
+  
+  )
---
+++ src/schedule/model.cljc
@@ -0,0 +1,142 @@
+(ns schedule.model
+  (:require [cljs.reader :as reader]
+            [re-frame.core :as rf]
+            [tick.core :as t]))
+
+(rf/reg-event-fx
+ ::set-text-area-value
+ (fn [{db :db} [_ value]]
+   (println value)
+   {:db (assoc-in db [:text-area :value] value)}))
+
+(rf/reg-event-fx
+ ::set-user-filter
+ (fn [{db :db} [_ uid]]
+   (let [user-ids (get-in db [:filter :user-id])]
+     (if (contains? user-ids uid)
+       {:db (update-in db [:filter :user-id] (fnil disj #{}) uid)}
+       {:db (update-in db [:filter :user-id] (fnil conj #{}) uid)}))))
+
+(rf/reg-event-fx
+ ::set-date-filter
+ (fn [{db :db} [_ period]]
+   {:db (assoc-in db [:filter :period] period)}))
+
+(rf/reg-event-fx
+ ::reset-user-filter
+ (fn [{db :db} _]
+   {:db (assoc-in db [:filter :user-id] #{})}))
+
+(rf/reg-sub
+ ::text-area-value
+ (fn [db _]
+   (get-in db [:text-area :value])))
+
+(rf/reg-sub
+ ::filters
+ (fn [db _]
+   (get-in db [:filter])))
+
+(rf/reg-sub
+ ::selected-user?
+ (fn [db [_ uid]]
+   (contains? (get-in db [:filter :user-id]) uid)))
+
+(rf/reg-sub
+ ::all-users-selected?
+ (fn [db _]
+   (empty? (get-in db [:filter :user-id]))))
+
+(rf/reg-sub
+ ::selected-period?
+ (fn [db [_ period]]
+   (= period (get-in db [:filter :period]))))
+
+(rf/reg-sub
+ ::selected-period
+ (fn [db _]
+   (get-in db [:filter :period])))
+
+(rf/reg-sub
+ ::text-area-edn-value
+ :<-[::text-area-value]
+ (fn [text _]
+   (try
+     (let [res (reader/read-string text)]
+       [res nil])
+     (catch js/Error e
+       (js/console.log e)
+       [nil (.-message e)]))))
+
+(defn today-date? [date]
+  (t/=
+   (t/date (t/now))
+   (t/date date)))
+
+(defn tomorrow-date? [date]
+  (t/=
+   (t/>> (t/date (t/now)) (t/new-period 1 :days))
+   (t/date date)))
+
+(defn next-week-date? [date]
+  (let [date-now (t/date (t/now))
+        date-now+7-days (t/>> date-now (t/new-period 7 :days))]
+    (t/<= date-now (t/date date) date-now+7-days)))
+
+(def period-mapping
+  {:today today-date?
+   :tomorrow tomorrow-date?
+   :next-week next-week-date?})
+
+(rf/reg-sub
+ ::text-area-filtered-edn-value
+ :<-[::text-area-value]
+ :<-[::filters]
+ (fn [[text filters] _]
+   (js/console.log "CALXS")
+   (try
+     (let [res (reader/read-string text)
+           period (:period filters)
+           period-match? (get period-mapping period identity)
+           user-id-param (:user-id filters)
+           user-id-match? (if (seq user-id-param)
+                            user-id-param
+                            any?)
+           _ (js/console.log period)
+           filtered (filter (fn [{:keys [user-id start end]}]
+                              (and (user-id-match? user-id)
+                                   (or (period-match? start)
+                                       (period-match? end)))) res)]
+       [filtered nil])
+     (catch js/Error e
+       (js/console.log e)
+       [nil (.-message e)]))))
+
+(rf/reg-sub
+ ::user-ids
+ :<- [::text-area-edn-value]
+ (fn [[records _] _]
+   (->> records
+        (map :user-id)
+        (distinct))))
+
+
+(comment
+
+  @(rf/subscribe [::filters])
+
+  @(rf/subscribe [::text-area-filtered-edn-value])
+  
+  (every-pred {:a 1 :b 10})
+
+  
+  
+  (filter [{:a 1 :b 10}
+           {:a 2 :b 10}
+           {:a 1 :b 12}
+           ])
+  (t/= (t/date (t/now))
+       (t/date #inst "2022-08-13T10:00:00.000Z"))
+  )
+
+
